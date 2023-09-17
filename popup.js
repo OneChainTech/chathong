ab2c8a4f98f6240d79e1b57b0338546c6f3be9ee
OneChainTech <44761375+OneChainTech@users.noreply.github.com>
2023-09-17 17:21:30 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,63 +1,99 @@
 // setting
 
 // 获取数据
-let apikey = localStorage.getItem('apikey');
-let modeltype = localStorage.getItem('modeltype');
+let apikey = localStorage.getItem("apikey");
+let modeltype = localStorage.getItem("modeltype");
 // console.log(apikey)
 
-if (localStorage.getItem('messagesinner')) {
-  document.getElementById('messages').innerHTML = localStorage.getItem('messagesinner');
+if (localStorage.getItem("messagesinner")) {
+  document.getElementById("messages").innerHTML = localStorage.getItem(
+    "messagesinner"
+  );
 }
 
-if (localStorage.getItem('questions')) {
-  document.getElementById('questions').innerHTML = localStorage.getItem('questions');
+if (localStorage.getItem("questions")) {
+  document.getElementById("questions").innerHTML = localStorage.getItem(
+    "questions"
+  );
 }
 
 function showOverlay() {
-  var overlay = document.getElementById('overlay');
-  overlay.classList.remove('hidden');
-  document.getElementById('apikey').value = apikey;
-  if (modeltype)
-    document.getElementById("gptVersions").value = modeltype;
+  var overlay = document.getElementById("overlay");
+  overlay.classList.remove("hidden");
+  document.getElementById("apikey").value = apikey;
+  if (modeltype) document.getElementById("gptVersions").value = modeltype;
 }
 
 function hideOverlay() {
-  var overlay = document.getElementById('overlay');
-  overlay.classList.add('hidden');
+  var overlay = document.getElementById("overlay");
+  overlay.classList.add("hidden");
 }
 
-document.getElementById('setting').addEventListener('click', function() {
+document.getElementById("setting").addEventListener("click", function () {
   showOverlay();
 });
-document.getElementById('setkey').addEventListener('click', function() {
-  apikey = document.getElementById('apikey').value;
+document.getElementById("setkey").addEventListener("click", function () {
+  apikey = document.getElementById("apikey").value;
   modeltype = document.getElementById("gptVersions").value;
   // 存储数据
-  localStorage.setItem('apikey', apikey);
-  localStorage.setItem('modeltype', modeltype);
+  localStorage.setItem("apikey", apikey);
+  localStorage.setItem("modeltype", modeltype);
 
   hideOverlay();
 });
 
-
 // prompt list
-const prompts = document.querySelectorAll('.prompt');
+const prompts = document.querySelectorAll(".prompt");
 
 prompts.forEach((prompt) => {
-  prompt.addEventListener('click', () => {
+  prompt.addEventListener("click", () => {
     const content = prompt.textContent;
     console.log(content);
-    document.getElementById('input').value = content;
+    document.getElementById("input").value = content;
   });
 });
 
+// hide prompt list
+var instruction = document.getElementById("instruction");
+var promptlist = document.getElementById("promptlist");
+instruction.onclick = function () {
+  if (promptlist.classList.contains("hidden")) {
+    promptlist.classList.remove("hidden");
+  } else {
+    promptlist.classList.add("hidden");
+  }
+};
+
+var input = document.getElementById("input");
+/*将“input_id”更改为相应输入标签的id*/
+
+input.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    // 回车键的键值为13
+    event.preventDefault();
+    // 防止标准的回车键行为发生，比如提交表单
+    document.getElementById("chat").click();
+  }
+});
+
+// var msgright = document.getElementById("messageshead");
+// msgright.onmouseover = function () {
+//   // this.style.fontWeight = "lighter";
+//   this.style.fontSize = "16px";
+// };
+
+// msgright.onmouseout = function () {
+//   // this.style.fontWeight = "normal";
+//   this.style.fontSize = "14px";
+// };
+
 // Add click event listeners to the buttons
-document.getElementById('copy').addEventListener('click', function() {
+document.getElementById("copy").addEventListener("click", function () {
   // 获取要复制的文本内容
-  const textToCopy = document.getElementById('messages').innerText;
+  const textToCopy = document.getElementById("messages").innerText;
 
   // 创建一个文本输入框元素
-  const input = document.createElement('input');
+  const input = document.createElement("input");
   // 设置文本内容
   input.value = textToCopy;
   // 添加到文档中
@@ -65,35 +101,28 @@ document.getElementById('copy').addEventListener('click', function() {
   // 选中文本
   input.select();
   // 执行复制命令
-  document.execCommand('copy');
+  document.execCommand("copy");
   // 从文档中移除文本输入框元素
   document.body.removeChild(input);
 });
 
-
 // Add click event listeners to the buttons
-document.getElementById('chat').addEventListener('click', function() {
+document.getElementById("chat").addEventListener("click", function () {
+  let question = document.getElementById("input").value;
 
-  let question = document.getElementById('input').value;
-
-  if (apikey === null || apikey === '') {
-
+  if (apikey === null || apikey === "") {
     showOverlay();
     return;
-
   } else {
+    if (question == "") return;
 
-    if (question == '') return;
-
-    document.getElementById('questions').innerHTML = question;
-    document.getElementById('messages').innerHTML = '正在分析...';
-    document.getElementById('input').value = '';
+    document.getElementById("questions").innerHTML = question;
+    document.getElementById("messages").innerHTML = "正在分析...";
+    document.getElementById("input").value = "";
 
     chatgpt(question);
     // flowise({ "question": question + "请用中文回答问题，如果不知道答案，请回复不太清楚" });
-
   }
-
 });
 
 async function chatgpt(question) {
@@ -106,35 +135,43 @@ async function chatgpt(question) {
   const payload = {
     model: modeltype,
     messages: [
-      { role: 'system', content: '你是一名AI助理，请用中文回答用户的问题，输出内容格式美观' },
-      { role: 'user', content: question }
+      {
+        role: "system",
+        content: "你是一名AI助理，请用中文回答用户的问题，输出内容格式美观"
+      },
+      { role: "user", content: question }
     ],
     stream: true
   };
 
   const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${OPENAI_API_KEY}`
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${OPENAI_API_KEY}`
   };
 
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
     headers,
     body: JSON.stringify(payload)
   });
 
-  document.getElementById('messages').innerHTML = "";
+  document.getElementById("messages").innerHTML = "";
 
   if (!response.body) return;
   const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
 
-  
-  localStorage.setItem('questions', document.getElementById('questions').innerHTML);
+  localStorage.setItem(
+    "questions",
+    document.getElementById("questions").innerHTML
+  );
   while (true) {
     var { value, done } = await reader.read();
     if (done) break;
 
-    const lines = value.toString().split("\n\n").filter((line) => line.trim() !== "");
+    const lines = value
+      .toString()
+      .split("\n\n")
+      .filter((line) => line.trim() !== "");
     let chunk = "";
     for (const line of lines) {
       const message = line.replace("data: ", "");
@@ -142,8 +179,11 @@ async function chatgpt(question) {
       chunk += JSON.parse(message).choices[0].delta.content;
     }
 
-    document.getElementById('messages').innerHTML += chunk;
-    localStorage.setItem('messagesinner', document.getElementById('messages').innerHTML);
+    document.getElementById("messages").innerHTML += chunk;
+    localStorage.setItem(
+      "messagesinner",
+      document.getElementById("messages").innerHTML
+    );
   }
 }
 
@@ -159,8 +199,7 @@ async function flowise(data) {
     }
   );
 
-
-  document.getElementById('messages').innerHTML = "";
+  document.getElementById("messages").innerHTML = "";
 
   if (!response.body) return;
   const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
@@ -168,7 +207,10 @@ async function flowise(data) {
     var { value, done } = await reader.read();
     if (done) break;
 
-    const lines = value.toString().split("\n\n").filter((line) => line.trim() !== "");
+    const lines = value
+      .toString()
+      .split("\n\n")
+      .filter((line) => line.trim() !== "");
     let chunk = "";
     for (const line of lines) {
       const message = line.replace("data: ", "");
@@ -176,7 +218,7 @@ async function flowise(data) {
       chunk += JSON.parse(message);
     }
 
-    document.getElementById('messages').innerHTML += chunk;
+    document.getElementById("messages").innerHTML += chunk;
   }
 
   // const result = await response.json();
