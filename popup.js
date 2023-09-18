d984898be88052fe3d0dbae56d5270bf7b6a802f
hong zheng <44761375+OneChainTech@users.noreply.github.com>
2023-09-18 12:23:28 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,15 +6,13 @@ let modeltype = localStorage.getItem("modeltype");
 // console.log(apikey)
 
 if (localStorage.getItem("messagesinner")) {
-  document.getElementById("messages").innerHTML = localStorage.getItem(
-    "messagesinner"
-  );
+  document.getElementById("messages").innerHTML =
+    localStorage.getItem("messagesinner");
 }
 
 if (localStorage.getItem("questions")) {
-  document.getElementById("questions").innerHTML = localStorage.getItem(
-    "questions"
-  );
+  document.getElementById("questions").innerHTML =
+    localStorage.getItem("questions");
 }
 
 function showOverlay() {
@@ -121,7 +119,6 @@ document.getElementById("chat").addEventListener("click", function () {
     document.getElementById("input").value = "";
 
     chatgpt(question);
-    // flowise({ "question": question + "请用中文回答问题，如果不知道答案，请回复不太清楚" });
     instruction.click();
   }
 });
@@ -130,7 +127,7 @@ async function chatgpt(question) {
   // const OPENAI_API_KEY = '
   const OPENAI_API_KEY = apikey;
 
-  const modeltype = document.getElementById("gptVersions").value;
+  // const modeltype = document.getElementById("gptVersions").value;
   console.log(modeltype);
 
   const payload = {
@@ -138,22 +135,22 @@ async function chatgpt(question) {
     messages: [
       {
         role: "system",
-        content: "你是一名AI助理，请用中文回答用户的问题，输出内容格式美观"
+        content: "你是一名AI助理，请用中文回答用户的问题，输出内容格式美观",
       },
-      { role: "user", content: question }
+      { role: "user", content: question },
     ],
-    stream: true
+    stream: true,
   };
 
   const headers = {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${OPENAI_API_KEY}`
+    Authorization: `Bearer ${OPENAI_API_KEY}`,
   };
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers,
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
   });
 
   document.getElementById("messages").innerHTML = "";
@@ -163,10 +160,12 @@ async function chatgpt(question) {
 
   localStorage.setItem(
     "questions",
-    document.getElementById("questions").innerHTML
+    document.getElementById("questions").innerHTML,
   );
   while (true) {
     var { value, done } = await reader.read();
+
+    console.log(done);
     if (done) break;
 
     const lines = value
@@ -183,45 +182,7 @@ async function chatgpt(question) {
     document.getElementById("messages").innerHTML += chunk;
     localStorage.setItem(
       "messagesinner",
-      document.getElementById("messages").innerHTML
+      document.getElementById("messages").innerHTML,
     );
   }
-}
-
-async function flowise(data) {
-  const response = await fetch(
-    "http://20.232.191.232:3000/api/v1/prediction/fc40f213-5a99-4832-ba88-9bff822b3f25",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }
-  );
-
-  document.getElementById("messages").innerHTML = "";
-
-  if (!response.body) return;
-  const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-  while (true) {
-    var { value, done } = await reader.read();
-    if (done) break;
-
-    const lines = value
-      .toString()
-      .split("\n\n")
-      .filter((line) => line.trim() !== "");
-    let chunk = "";
-    for (const line of lines) {
-      const message = line.replace("data: ", "");
-      if (message === "[DONE]") return;
-      chunk += JSON.parse(message);
-    }
-
-    document.getElementById("messages").innerHTML += chunk;
-  }
-
-  // const result = await response.json();
-  // return result;
 }
